31019614037c7ee4c25d2820af88d3294763b576
unknown <jimmymhuang@gmail.com>
2022-12-07 19:45:38 -0600

    edited links tabs

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,22 +1,62 @@
 import React from 'react'
 import {FiGithub, FiLinkedin} from 'react-icons/fi';
 import {SiGmail} from 'react-icons/si';
-import {} from 'react-icons';
+import {MdPerson} from 'react-icons/md';
 
 const Links = () => {
-  return (
-    <div className="flex flex-row bottom-0 fixed text-white">
-        <ul>
-            <li className="flex justify-between items-center w-40 h-1/4 px-4">
-                <a href="">
-                    <>
-                        LinkedIn <FiLinkedin />
-                        GitHub <FiGithub />
-                        Email <SiGmail />
 
-                    </>
+    const links = [
+        {
+            id: 1,
+            child: (
+                <>
+                    LinkedIn <FiLinkedin size={25}/>
+                </>
+            ),
+            href: 'https://www.linkedin.com/in/jimmy-huang-04468797/',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                    GitHub <FiGithub size={25}/>
+                </>
+            ),
+            href: 'https://github.com/jimmehhuang',
+        },
+        {
+            id: 3,
+            child: (
+                <>
+                    Mail <SiGmail size={25}/>
+                </>
+            ),
+            href: 'mailto:jimmymhuang@gmail.com',
+        },
+        {
+            id: 4,
+            child: (
+                <>
+                    Resume <MdPerson size={25}/>
+                </>
+            ),
+            href: '/resume.pdf',
+            style: 'rounded-br-md',
+            download: true
+        }
+    ]
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+        <ul>
+            {links.map(({id, child, href, style, download}) => (
+                <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  bg-gray-700" + " " + style}>
+                <a href={href} className="flex justify-between items-center w-full text-white" download={download} target="_blank" rel="noreferrer">
+                    {child}
                 </a>
             </li>
+            ))}
         </ul>
     </div>
   )
