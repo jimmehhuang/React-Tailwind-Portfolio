9902ed7f908316608b585089cc18f86f645ef92c
unknown <jimmymhuang@gmail.com>
2022-12-07 23:27:31 -0600

    moved link tabs to other side

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -10,7 +10,7 @@ const Links = () => {
             id: 1,
             child: (
                 <>
-                    LinkedIn <FiLinkedin size={25}/>
+                    <FiLinkedin size={25}/>
                 </>
             ),
             href: 'https://www.linkedin.com/in/jimmy-huang-04468797/',
@@ -20,7 +20,7 @@ const Links = () => {
             id: 2,
             child: (
                 <>
-                    GitHub <FiGithub size={25}/>
+                    <FiGithub size={25}/>
                 </>
             ),
             href: 'https://github.com/jimmehhuang',
@@ -29,7 +29,7 @@ const Links = () => {
             id: 3,
             child: (
                 <>
-                    Mail <SiGmail size={25}/>
+                    <SiGmail size={25}/>
                 </>
             ),
             href: 'mailto:jimmymhuang@gmail.com',
@@ -38,7 +38,7 @@ const Links = () => {
             id: 4,
             child: (
                 <>
-                    Resume <MdPerson size={25}/>
+                    <MdPerson size={25}/>
                 </>
             ),
             href: '/resume.pdf',
@@ -48,10 +48,10 @@ const Links = () => {
     ]
 
   return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden:flex flex-col top-[55%] right-0 fixed">
         <ul>
             {links.map(({id, child, href, style, download}) => (
-                <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  bg-gray-700" + " " + style}>
+                <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 mr-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  bg-gray-700" + " " + style}>
                 <a href={href} className="flex justify-between items-center w-full text-white" download={download} target="_blank" rel="noreferrer">
                     {child}
                 </a>
