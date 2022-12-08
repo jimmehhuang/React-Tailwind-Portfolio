38f15fc1b560da89422e7fb928a3b2b24bfa061b
unknown <jimmymhuang@gmail.com>
2022-12-07 23:57:51 -0600

    created About.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Links from "./components/Links";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Home />
+      <About />
 
       <Links />
     </div>
