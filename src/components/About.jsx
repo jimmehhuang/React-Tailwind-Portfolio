c274267f136d197578d5cd513eb2876263c75325
unknown <jimmymhuang@gmail.com>
2022-12-08 22:02:19 -0600

    added portfolio jsx section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
-            <p>Etcetcetcetc</p>
+            <p className="text-xl">Etcetcetcetc</p>
             <br/>
-            <p>More etcetcetc</p>
+            <p className="text-xl">More etcetcetc</p>
         </div>
     </div>
   )
