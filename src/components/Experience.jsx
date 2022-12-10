81bd36c97cac8e6dd547b21c3934c419784c7470
unknown <jimmymhuang@gmail.com>
2022-12-09 18:41:30 -0600

    further edits to experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import html from '../assets/images/html'
-import css from '../assets/images/css'
-import javascript from '../assets/images/javascript'
-import nodejs from '../assets/images/nodejs'
-import github from '../assets/images/github'
-import reactimage from '../assets/images/react'
-import mongoDB from '../assets/images/mongoDB'
-import mysql from '../assets/images/mysql'
-import tailwind from '../assets/images/tailwind'
+import html from '../assets/images/html.png'
+import css from '../assets/images/css.png'
+import javascript from '../assets/images/javascript.png'
+import nodejs from '../assets/images/node.png'
+import github from '../assets/images/github.png'
+import reactimage from '../assets/images/react.png'
+import mongoDB from '../assets/images/mongoDB.png'
+import mysql from '../assets/images/mysql.png'
+import tailwind from '../assets/images/tailwind.png'
 
 // import images of the stuff
 
@@ -23,50 +23,50 @@ const Experience = () => {
         {
             id: 2,
             src: css,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'CSS',
+            style: 'shadow-blue-500'
         },
         {
             id: 3,
             src: javascript,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'JavaScript',
+            style: 'shadow-yellow-500'
         },
         {
             id: 4,
             src: nodejs,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'NodeJS',
+            style: 'shadow-green-500'
         },
         {
             id: 5,
             src: github,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'GitHub',
+            style: 'shadow-gray-500'
         },
         {
             id: 6,
             src: reactimage,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'ReactJS',
+            style: 'shadow-blue-500'
         },
         {
             id: 7,
             src: mongoDB,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'MongoDB',
+            style: 'shadow-green-500'
         },
         {
             id: 8,
             src: mysql,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'MySQL',
+            style: 'shadow-blue-500'
         },
         {
             id: 9,
             src: tailwind,
-            title: 'HTML',
-            style: 'shadow-orange-500'
+            title: 'Tailwind CSS',
+            style: 'shadow-sky-400'
         }
     ]
 
@@ -77,14 +77,17 @@ const Experience = () => {
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
                 <p className='py-6'>These are some of the technologies I've worked with</p>
             </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+            {experiences.map(({id, src, title, style}) => (
+                <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                    <img src={src} alt="" className='w-20 mx-auto'/>
+                    <p className='mt-4'>{title}</p>
+                </div>
+            ))}                  
+        </div>
         </div>
 
-        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-            <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
-                <img src="" alt="" className='w-20 mx-auto'/>
-                <p className='mt-4'>HTML</p>
-            </div>
-        </div>
+        
     </div>
   )
 }
